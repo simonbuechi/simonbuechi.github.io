74fdf5a1dc828482a156e0394f11498f666102e1
Simon Buechi <simon.buechi@gmail.com>
2020-04-04 08:43:53 +0200

    add digitalgv test environment

---
+++ digitalgv/precache-manifest.0c9a836dcd25cf783a6e6b1db033454a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4a4a1f4f6bd23dd3838d62dfdd05649",
+    "url": "/digitalgv/index.html"
+  },
+  {
+    "revision": "95df151f2102372c294a",
+    "url": "/digitalgv/static/css/2.feac8f0d.chunk.css"
+  },
+  {
+    "revision": "422c27ff153ed90e1726",
+    "url": "/digitalgv/static/css/main.4bb1c32d.chunk.css"
+  },
+  {
+    "revision": "95df151f2102372c294a",
+    "url": "/digitalgv/static/js/2.cdb58131.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/digitalgv/static/js/2.cdb58131.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "422c27ff153ed90e1726",
+    "url": "/digitalgv/static/js/main.fde2f2bf.chunk.js"
+  },
+  {
+    "revision": "907ee462d1fd5530bd65",
+    "url": "/digitalgv/static/js/runtime-main.fa5b7da6.js"
+  },
+  {
+    "revision": "94df564552b22441f358627dbf24bf7e",
+    "url": "/digitalgv/static/media/roboto-slab-latin-100.94df5645.woff2"
+  },
+  {
+    "revision": "e79adfbe66816f5035985c34fddd1a50",
+    "url": "/digitalgv/static/media/roboto-slab-latin-100.e79adfbe.woff"
+  },
+  {
+    "revision": "cee5cfd9886336b0e1bf0e288a15c49d",
+    "url": "/digitalgv/static/media/roboto-slab-latin-300.cee5cfd9.woff2"
+  },
+  {
+    "revision": "eb419554ada80071e0033414508babec",
+    "url": "/digitalgv/static/media/roboto-slab-latin-300.eb419554.woff"
+  },
+  {
+    "revision": "7bb3d04f8cca3e83d2c28562a1ae3503",
+    "url": "/digitalgv/static/media/roboto-slab-latin-400.7bb3d04f.woff"
+  },
+  {
+    "revision": "e9b293be36045a187354c52e4c9105ee",
+    "url": "/digitalgv/static/media/roboto-slab-latin-400.e9b293be.woff2"
+  },
+  {
+    "revision": "00a8dd06c3085ca4a620dd6f2fada497",
+    "url": "/digitalgv/static/media/roboto-slab-latin-700.00a8dd06.woff2"
+  },
+  {
+    "revision": "a977a6cbb71e7fc7d34c4b4ba7fb6197",
+    "url": "/digitalgv/static/media/roboto-slab-latin-700.a977a6cb.woff"
+  }
+]);
