f2bc6eddd23a34ca63311a1b58fb70a1ff48fef5
Simon Buechi <simon.buechi@gmail.com>
2020-06-15 09:40:05 +0200

    release general update

---
+++ precache-manifest.3d8ea83b5b6b85416c4daa26beb1d4bf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abcd3ee273d26252f12be64900dceff1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c65f084b8cd7f3be28fd",
+    "url": "./static/css/main.347ffa71.chunk.css"
+  },
+  {
+    "revision": "39a647b075be2baef765",
+    "url": "./static/js/2.8fd4c87b.chunk.js"
+  },
+  {
+    "revision": "8da610e2f8e9068dd57cd296e0187145",
+    "url": "./static/js/2.8fd4c87b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c65f084b8cd7f3be28fd",
+    "url": "./static/js/main.91e0643e.chunk.js"
+  },
+  {
+    "revision": "6d558db7819d307dca8d",
+    "url": "./static/js/runtime-main.057117bc.js"
+  },
+  {
+    "revision": "a2df611a79a30d34d14fe797b9888444",
+    "url": "./static/media/Basetica-Med.a2df611a.otf"
+  },
+  {
+    "revision": "a25047c6bb4d777a78e3475c19dece61",
+    "url": "./static/media/simonbuechi-landscape-medium.a25047c6.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.3839ce998bffb40d7632dc0401af4467.js"
+  "./precache-manifest.3d8ea83b5b6b85416c4daa26beb1d4bf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
